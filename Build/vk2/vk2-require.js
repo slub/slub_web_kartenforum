c17f6ea5efacac99587be8012b1d4cd50722251f
Alexander Bigga <alexander.bigga@slub-dresden.de>
2021-03-04 21:21:01 +0100

    Remove welcomepageapp.js

--- Build/vk2/vk2-require.js
+++ Build/vk2/vk2-require.js
@@ -6,6 +6,5 @@ goog.require('vk2.app.GeoreferenceChooseApp');
 goog.require('vk2.app.MapProfileApp');
 goog.require('vk2.app.PresentationApp');
 goog.require('vk2.app.UserHistoryApp');
-goog.require('vk2.app.WelcomePageApp');
 goog.require('vk2.settings');
 goog.require('vk2.utils');
