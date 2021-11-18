689ec48ba156fd43298a8173cac5cf1332bb68f6
Thomas Jung <thomas@jung.digital>
2021-11-18 21:06:01 +0100

    Implement some extension configuration variables and pass them to Fluid templates

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,6 @@
 <?php
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use \TYPO3\CMS\Core\Utility\GeneralUtility;
 use \TYPO3\CMS\Core\Imaging\IconRegistry;
 use \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
@@ -23,3 +24,9 @@ foreach ($icons as $icon) {
         ['source' => 'EXT:slub_web_kartenforum/Resources/Public/Icons/Backend/' . $icon . '.svg']
     );
 }
+
+/***************
+ * Pass some extension configuration variables to typoscript constants
+ */
+ExtensionManagementUtility::addTypoScriptConstants("plugin.tx_slubwebkartenforum.settings.georefAuthUser = " . $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['slub_web_kartenforum']['georefAuthUser']);
+ExtensionManagementUtility::addTypoScriptConstants("plugin.tx_slubwebkartenforum.settings.georefAuthPassword = " . $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['slub_web_kartenforum']['georefAuthPassword']);
