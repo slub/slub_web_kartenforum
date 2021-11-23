4e67068c355fcd8531335b1a18f18f85a9b8e6b7
Jacob Mendt <jacob.mendt@pikobytes.de>
2021-11-23 13:51:54 +0100

    Update partial templates for map and georeferencer plugin

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use \TYPO3\CMS\Core\Utility\GeneralUtility;
 use \TYPO3\CMS\Core\Imaging\IconRegistry;
 use \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
