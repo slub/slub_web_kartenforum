694ce56360a19ec678327b34d04ce166410f1f9d
Alexander Bigga <alexander.bigga@slub-dresden.de>
2022-04-26 13:48:50 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,5 +26,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Alexander Bigga',
     'author_email' => 'typo3@slub-dresden.de',
     'author_company' => 'SLUB Dresden',
-    'version' => '2.0.0',
+    'version' => '2.0.1',
 ];
