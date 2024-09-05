9f13e447d11a1d290435bdfbbeec397bf1c89c7a
Nicolas Looschen <nicolas.looschen@pikobytes.de>
2024-09-05 15:39:37 +0200

    Change watch mode in map application to rebuild on style change

--- Build/esbuild.map.js
+++ Build/esbuild.map.js
@@ -6,13 +6,15 @@
  */
 import esbuild from "esbuild";
 import sass from "esbuild-plugin-sass";
+import watcher from "@parcel/watcher";
 
 // Define constants
 const outputDir = "../Resources/Public/Build/";
 const isWatch = process.env.MODE === "watch";
 const isProduction = process.env.NODE_ENV === "production";
 
-esbuild
+let time = new Date();
+let result = esbuild
     .build({
         bundle: true,
         entryPoints: ["./src/apps/map/index.jsx"],
@@ -24,15 +26,41 @@ esbuild
         },
         minify: !isWatch && isProduction,
         target: "es6",
-        plugins: [sass()],
+        plugins: [sass({ cache: true })],
         outfile: `${outputDir}plugin-map.js`,
-        watch: isWatch
-            ? {
-                  onRebuild(error, result) {
-                      if (error) console.error("watch build failed:", error);
-                      else console.log("watch build succeeded:", result);
-                  },
-              }
-            : false,
+        incremental: isWatch,
+        // watch: isWatch
+        //     ? {
+        //           onRebuild(error, result) {
+        //               if (error) console.error("watch build failed:", error);
+        //               else console.log("watch build succeeded:", result);
+        //           },
+        //       }
+        //     : false,
+    })
+    .then((r) => {
+        console.log(
+            "Build succeeded in: " + ((Date.now() - time) / 1000).toFixed(2)
+        );
+        return r;
     })
     .catch(() => process.exit(1));
+
+if (isWatch) {
+    const subscription = await watcher.subscribe(
+        process.cwd(),
+        (err, events) => {
+            time = new Date();
+            result = result
+                .then((r) => r.rebuild())
+                .then((r) => {
+                    console.log(
+                        "Rebuild. Elapsed time:" +
+                            ((Date.now() - time) / 1000).toFixed(2),
+                        { errors: r.errors, warning: r.warnings }
+                    );
+                    return r;
+                });
+        }
+    );
+}
