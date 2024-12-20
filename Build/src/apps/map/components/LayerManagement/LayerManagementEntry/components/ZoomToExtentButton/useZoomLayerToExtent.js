255c2949b3e73392011defa788f1ba66400f123a
Tom Schulze <tom.schulze@pikobytes.de>
2024-12-20 11:45:54 +0100

    Add padding when zooming to extent, add fix for vertical layout

--- Build/src/apps/map/components/LayerManagement/LayerManagementEntry/components/ZoomToExtentButton/useZoomLayerToExtent.js
+++ Build/src/apps/map/components/LayerManagement/LayerManagementEntry/components/ZoomToExtentButton/useZoomLayerToExtent.js
@@ -6,7 +6,7 @@
  */
 
 import { useRecoilCallback } from "recoil";
-import { mapState } from "@map/atoms";
+import { layoutState, mapState } from "@map/atoms";
 import { METADATA } from "@map/components/CustomLayers";
 
 import {
@@ -14,32 +14,44 @@ import {
     SPATIALTEMPORALSEARCH,
 } from "@map/components/PaginatingDataController/util";
 import { isDefined } from "@util/util";
+import { LAYOUT_TYPES } from "@map/layouts/util";
 
-const OFFSET = { left: 10, right: 10, top: 50, bottom: 50 };
+const SCREEN_PANEL_GAP = 24;
+const EXTRA_PADDING = 100;
 
-const PADDING = {
-    left: SPATIALTEMPORALSEARCH.width + OFFSET.left,
-    right: LAYERMANAGEMENT.width + OFFSET.right,
-    top: OFFSET.top,
-    bottom: OFFSET.bottom,
+const PADDING_HORIZONTAL = {
+    left: SCREEN_PANEL_GAP + SPATIALTEMPORALSEARCH.width + EXTRA_PADDING,
+    right: SCREEN_PANEL_GAP + LAYERMANAGEMENT.width + EXTRA_PADDING,
+    top: 60, // SCREEN_PANEL_GAP + marker image height
+    bottom: SCREEN_PANEL_GAP,
 };
 
-const FIT_TO_BOUNDS_OPTIONS = {
-    padding: PADDING,
+const PADDING_VERTICAL = {
+    left: 50,
+    right: 50,
+    top: 75, // top control
+    bottom: 150, // layer mangagement button,
+};
+
+const getOptions = (layout) => ({
+    padding:
+        layout === LAYOUT_TYPES.HORIZONTAL
+            ? PADDING_HORIZONTAL
+            : PADDING_VERTICAL,
     animate: false,
-};
+});
 
 const useZoomLayerToExtent = () => {
     const zoomToExtent = useRecoilCallback(
         ({ snapshot }) =>
             async (layer) => {
                 const map = await snapshot.getPromise(mapState);
+                const layout = await snapshot.getPromise(layoutState);
+                const options = getOptions(layout);
 
                 if (isDefined(map)) {
                     const extent = layer.getMetadata(METADATA.bounds);
-                    // add percentage based padding
-                    //@TODO: Adjust for mobile layout
-                    map.fitBounds(extent, FIT_TO_BOUNDS_OPTIONS);
+                    map.fitBounds(extent, options);
                 }
             },
         []
