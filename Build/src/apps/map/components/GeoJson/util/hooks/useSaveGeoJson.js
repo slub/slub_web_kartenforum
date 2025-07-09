slub/slub_web_kartenforum

commit 3a661329563e23697c440db1aa2788f31bd4103a

Allow time range for vector map feature property 'time' (#483)
* Allow time ranges for a feature's time property

* Fix time property is not defined

* Add time range filtering for IDOHIST layer

* Refactor GeoJsonFeatureEditPanel
- use html form and react-hook-form
- add time period field

* Refactor GeoJsonEditField css

* Rename EditStyleField to StyleField

* Add error styling

* Fix time field error styling

* Fix time period field validation

* Remove console.log

* Fix legacy marker value not displayed in form

* Adapt change tracker for time array

* Adapt useGeoJsonFeature for time array

* Use const instead of let

Build/src/apps/map/components/GeoJson/util/util.js
@@ -7,12 +7,7 @@
 
 import bbox from "@turf/bbox";
 
-import {
-    ignoredProperties,
-    predefinedProperties,
-    stylingProperties,
-    styleFieldSettings,
-} from "../constants.js";
+import { ignoredProperties, stylingProperties } from "../constants.js";
 
 import { isDefined } from "@util/util.js";
 import {
@@ -29,153 +24,6 @@ import {
 } from "@map/layouts/util";
 
 import { METADATA } from "@map/components/CustomLayers";
-
-/**
- * A utility function to merge an array of entries with values from an existing Object.
- * To be used as a callback for Object.entries(myObj).map().
- * @param {object} obj
- * @returns {[string, unknown][]}
- */
-const mergeWith =
-    (obj) =>
-    ([key, originalValue]) => {
-        const mergedValue = obj[key];
-        if (
-            Object.hasOwn(obj, key) &&
-            isDefined(mergedValue) &&
-            mergedValue !== ""
-        ) {
-            return [key, mergedValue];
-        }
-
-        return [key, originalValue];
-    };
-
-const getGeometryType = (feature) => {
-    return feature?.geometry?.type;
-};
-
-const getDeclaredGeometryTypes = () => {
-    const geometryTypesFromStylingProperties = Object.values(
-        styleFieldSettings
-    ).map(({ geometryTypes }) => geometryTypes);
-
-    const types = new Set(geometryTypesFromStylingProperties.flat(2));
-
-    return types;
-};
-
-const checkForStylingEntries = (() => {
-    const geometryTypes = getDeclaredGeometryTypes();
-
-    const checkForStylingEntriesPerGeometryType = {};
-    for (const geometryType of geometryTypes) {
-        checkForStylingEntriesPerGeometryType[geometryType] = ([
-            propertyKey,
-        ]) => {
-            return (
-                stylingProperties.includes(propertyKey) &&
-                styleFieldSettings[propertyKey].geometryTypes.includes(
-                    geometryType
-                )
-            );
-        };
-    }
-
-    return checkForStylingEntriesPerGeometryType;
-})();
-
-const filterPredefinedEntries = ([propertyKey]) =>
-    !predefinedProperties.includes(propertyKey);
-
-const filterIgnoredEntries = ([propertyKey]) =>
-    !ignoredProperties.includes(propertyKey);
-
-export const convertIntToHex = (int) => {
-    return Number(int.toFixed(0)).toString(16).padStart(2, "0");
-};
-
-/**
- * Filter out styling properties
- * @param feature
- * @return {object}
- */
-const filterStylingProperties = (feature) => {
-    const geometryType = getGeometryType(feature);
-    const entries = Object.entries(feature.properties);
-
-    const filteredEntries = entries.filter(
-        (entry) => !checkForStylingEntries[geometryType](entry)
-    );
-
-    return Object.fromEntries(filteredEntries);
-};
-
-/**
- * Retain styling properties
- * @param feature
- * @return {object}
- */
-const retainStylingProperties = (feature) => {
-    const geometryType = getGeometryType(feature);
-    const entries = Object.entries(feature.properties);
-
-    const filteredEntries = entries.filter((entry) =>
-        checkForStylingEntries[geometryType](entry)
-    );
-
-    return Object.fromEntries(filteredEntries);
-};
-
-/**
- * Create an array of entries from the GeoJSON feature properties.
- * Only styling properties are included.
- * Default styling properties are merged with feature properties.
- *
- * @param feature
- * @return {[string,*][]}
- */
-export const extractStyleProperties = (feature) => {
-    const geometryType = getGeometryType(feature);
-    const styleProperties = retainStylingProperties(feature);
-
-    const defaultStyleEntries = Object.entries(styleFieldSettings)
-        .filter(checkForStylingEntries[geometryType])
-        .map(([key, { default: defaultValue }]) => [
-            key,
-            defaultValue(geometryType),
-        ]);
-
-    return defaultStyleEntries.map(mergeWith(styleProperties));
-};
-
-/**
- * Create an array of entries from the GeoJSON feature properties.
- * Styling properties are filtered out.
- * Default/Predefined entries are merged with feature properties.
- * Predefined entries come first and are sorted.
- *
- * @param feature
- * @return {[string,*][]}
- */
-export const extractAndSortNonStyleProperties = (feature) => {
-    const nonStylingProperties = filterStylingProperties(feature);
-
-    const defaultSortedPredefinedEntries = predefinedProperties.map((key) => [
-        key,
-        "",
-    ]);
-
-    const mergedSortedPredefinedEntries = defaultSortedPredefinedEntries.map(
-        mergeWith(nonStylingProperties)
-    );
-
-    const customEntries = Object.entries(nonStylingProperties)
-        .filter(filterPredefinedEntries)
-        .filter(filterIgnoredEntries);
-
-    return [...mergedSortedPredefinedEntries, ...customEntries];
-};
 
 /*
  * filter and extract the properties of the feature
@@ -218,24 +66,19 @@ const getPropertyKeysToBeRemoved = (oldProperties, newProperties) => {
     return removedPropertyKeys;
 };
 
-export const buildGeoJSONSourceDiff = ({
-    oldProperties,
-    propertyFields,
-    styleFields,
-}) => {
+export const buildGeoJSONSourceDiff = ({ oldProperties, newProperties }) => {
     const updatedProperties = {};
-    propertyFields.forEach(([key, value]) => {
+
+    for (const [key, value] of Object.entries(newProperties)) {
         const isEmptyValue = value === "";
         const isEmptyKey = key === "";
-        if (isDefined(key) && !isEmptyKey && !isEmptyValue) {
-            updatedProperties[key] = value;
+        if (!isDefined(key) || isEmptyKey || isEmptyValue) {
+            continue;
         }
-    });
-    styleFields.forEach(([key, value]) => {
-        updatedProperties[key] = value;
-    });
 
-    const newProperties = Object.fromEntries(propertyFields);
+        updatedProperties[key] = value;
+    }
+
     const removedPropertyKeys = getPropertyKeysToBeRemoved(
         oldProperties,
         newProperties
@@ -251,22 +94,6 @@ export const buildGeoJSONSourceDiff = ({
     };
 
     return geoJSONSourceDiff;
-};
-
-export const validatePropertyField = (key, value, existingFields) => {
-    const errors = { key: false, value: false };
-    const isKeyExists = existingFields.some(
-        ([existingKey]) => existingKey.toLowerCase() === key.toLowerCase()
-    );
-    if (!key.trim() || isKeyExists) {
-        errors.key = true;
-    }
-    if (!value.trim()) errors.value = true;
-
-    return {
-        isValid: !errors.key && !errors.value,
-        errors,
-    };
 };
 
 export const exitDrawMode = (set) => {

Build/src/apps/map/components/CustomLayers/GeoJsonLayer/util.js
@@ -5,7 +5,7 @@
  * file "LICENSE.txt", which is part of this source code package.
  */
 
-import { isDefined } from "@util/util";
+import { isDefined, isString } from "@util/util";
 import { parseDateIso, formatDateIso, normalizeDate } from "@util/date";
 import { FEATURE_PROPERTIES } from "@map/components/GeoJson/constants";
 
@@ -45,9 +45,9 @@ export const convertFeatureForApplicationState = (feature) => {
 const convertForApplicationState = (properties) => {
     const time = properties[FEATURE_PROPERTIES.time] ?? "";
 
-    const parsedTime = normalizeDate(parseDateIso(time)).valueOf();
+    const parsedTime = convertTimeToApplicationState(time);
 
-    if (Number.isNaN(parsedTime)) {
+    if (parsedTime.length === 0) {
         delete properties[FEATURE_PROPERTIES.time];
         return properties;
     }
@@ -63,9 +63,9 @@ const convertForApplicationState = (properties) => {
 const convertForPersistenceState = (properties) => {
     const time = properties[FEATURE_PROPERTIES.time] ?? "";
 
-    const parsedTime = formatDateIso(time);
+    const parsedTime = convertTimeToPersistenceState(time);
 
-    if (parsedTime === "") {
+    if (parsedTime.length === 0) {
         delete properties[FEATURE_PROPERTIES.time];
         return properties;
     }
@@ -76,6 +76,58 @@ const convertForPersistenceState = (properties) => {
     };
 
     return newProperties;
+};
+
+const convertTimeToApplicationState = (time) => {
+    if (Array.isArray(time)) {
+        if (time.length !== 2) {
+            return [];
+        }
+
+        const parsedStart = normalizeDate(parseDateIso(time[0])).valueOf();
+        const parsedEnd = normalizeDate(parseDateIso(time[1])).valueOf();
+
+        if (Number.isNaN(parsedStart) || Number.isNaN(parsedEnd)) {
+            return [];
+        }
+
+        if (parsedStart > parsedEnd) {
+            return [parsedEnd, parsedStart];
+        }
+
+        return [parsedStart, parsedEnd];
+    }
+
+    if (isString(time)) {
+        const parsedTime = normalizeDate(parseDateIso(time)).valueOf();
+
+        if (Number.isNaN(time)) {
+            return [];
+        }
+
+        return [parsedTime, parsedTime];
+    }
+
+    return [];
+};
+
+const convertTimeToPersistenceState = (time) => {
+    if (!Array.isArray(time)) {
+        return [];
+    }
+
+    if (time.length !== 2) {
+        return [];
+    }
+
+    const parsedStart = formatDateIso(time[0]);
+    const parsedEnd = formatDateIso(time[1]);
+
+    if (parsedStart === "" || parsedEnd === "") {
+        return [];
+    }
+
+    return [parsedStart, parsedEnd];
 };
 
 /**.
@@ -99,8 +151,8 @@ export const getTimeFilter = (timeExtent) => {
         ["!", ["has", FEATURE_PROPERTIES.time]],
         [
             "all",
-            [">=", ["get", FEATURE_PROPERTIES.time], min * 1000],
-            ["<=", ["get", FEATURE_PROPERTIES.time], max * 1000],
+            ["<=", ["at", 0, ["get", FEATURE_PROPERTIES.time]], max * 1000],
+            [">=", ["at", 1, ["get", FEATURE_PROPERTIES.time]], min * 1000],
         ],
     ];
 };

Build/src/apps/map/components/GeoJson/GeoJsonFeatureEditPanel/FeaturePropertiesForm/util.js
@@ -1,0 +1,306 @@
+/*
+ * Created by tom.schulze@pikobytes.de on 23.06.25.
+ *
+ * This file is subject to the terms and conditions defined in
+ * file "LICENSE.txt", which is part of this source code package.
+ */
+
+import { isDefined } from "@util/util";
+import { parseColorStringToHexWithLegacy } from "../../util/colorUtil";
+import {
+    DEFAULT_STYLE_VALUES,
+    GEOJSON_LAYER_TYPES,
+} from "@map/components/CustomLayers/GeoJsonLayer/VkfMapInteractionStrategy/constants";
+import {
+    ignoredProperties,
+    predefinedProperties,
+    styleFieldSettings,
+    stylingProperties,
+} from "../../constants";
+import { formatDateLocalized, parseDateLocalized } from "@util/date.js";
+
+/**
+ * A utility function to merge an array of entries with values from an existing Object.
+ * To be used as a callback for Object.entries(myObj).map().
+ * @param {object} obj
+ * @returns {[string, unknown][]}
+ */
+const mergeWith =
+    (obj) =>
+    ([key, originalValue]) => {
+        const mergedValue = obj[key];
+        if (
+            Object.hasOwn(obj, key) &&
+            isDefined(mergedValue) &&
+            mergedValue !== ""
+        ) {
+            return [key, mergedValue];
+        }
+
+        return [key, originalValue];
+    };
+
+const getGeometryType = (feature) => {
+    return feature?.geometry?.type;
+};
+
+const getDeclaredGeometryTypes = () => {
+    const geometryTypesFromStylingProperties = Object.values(
+        styleFieldSettings
+    ).map(({ geometryTypes }) => geometryTypes);
+
+    const types = new Set(geometryTypesFromStylingProperties.flat(2));
+
+    return types;
+};
+
+const checkForStylingEntries = (() => {
+    const geometryTypes = getDeclaredGeometryTypes();
+
+    const checkForStylingEntriesPerGeometryType = {};
+    for (const geometryType of geometryTypes) {
+        checkForStylingEntriesPerGeometryType[geometryType] = ([
+            propertyKey,
+        ]) => {
+            return (
+                stylingProperties.includes(propertyKey) &&
+                styleFieldSettings[propertyKey].geometryTypes.includes(
+                    geometryType
+                )
+            );
+        };
+    }
+
+    return checkForStylingEntriesPerGeometryType;
+})();
+
+const filterPredefinedEntries = ([propertyKey]) =>
+    !predefinedProperties.includes(propertyKey);
+
+const filterIgnoredEntries = ([propertyKey]) =>
+    !ignoredProperties.includes(propertyKey);
+
+/**
+ * Filter out styling properties
+ * @param feature
+ * @return {object}
+ */
+const filterStylingProperties = (feature) => {
+    const geometryType = getGeometryType(feature);
+    const entries = Object.entries(feature.properties);
+
+    const filteredEntries = entries.filter(
+        (entry) => !checkForStylingEntries[geometryType](entry)
+    );
+
+    return Object.fromEntries(filteredEntries);
+};
+
+/**
+ * Retain styling properties
+ * @param feature
+ * @return {object}
+ */
+const retainStylingProperties = (feature) => {
+    const geometryType = getGeometryType(feature);
+    const entries = Object.entries(feature.properties);
+
+    const filteredEntries = entries.filter((entry) =>
+        checkForStylingEntries[geometryType](entry)
+    );
+
+    return Object.fromEntries(filteredEntries);
+};
+
+/**
+ * Create an array of records from the GeoJSON feature properties used for map styles.
+ * Only styling properties are included.
+ * Default styling properties are merged with feature properties.
+ *
+ * @param feature
+ * @return {[string,*][]}
+ */
+export const extractStyleProperties = (feature) => {
+    const geometryType = getGeometryType(feature);
+    const styleProperties = retainStylingProperties(feature);
+
+    const defaultStyleEntries = Object.entries(styleFieldSettings)
+        .filter(checkForStylingEntries[geometryType])
+        .map(([key, { default: defaultValue }]) => [
+            key,
+            defaultValue(geometryType),
+        ]);
+
+    const styleEntries = defaultStyleEntries.map(mergeWith(styleProperties));
+
+    return styleEntries.map(([key, value]) => ({ name: key, value }));
+};
+
+// maps [number,number] to localized date format for display in form fields
+const propertiesTimePeriodToForm = (timePeriod) => {
+    const timeStart = formatDateLocalized(timePeriod[0]) ?? "";
+    const timeEnd = formatDateLocalized(timePeriod[1]) ?? "";
+
+    return { timeStart, timeEnd };
+};
+
+// maps localized [string,string] to [number,number]
+const formTimePeriodToProperties = (start, end) => {
+    if (start === "" || end === "") {
+        return [];
+    }
+
+    const parsedStartDate = parseDateLocalized(start).valueOf();
+    const parsedEndDate = parseDateLocalized(end).valueOf();
+
+    if (Number.isNaN(parsedStartDate) || Number.isNaN(parsedEndDate)) {
+        return [];
+    }
+
+    return [parsedStartDate, parsedEndDate];
+};
+
+/**
+ * Maps feature properties to form properties
+ *
+ * @param feature
+ * @return {Object.<string, any>}
+ */
+export const featurePropertiesToFormProperties = (feature) => {
+    const nonStylingProperties = filterStylingProperties(feature);
+    const defaultPredefinedEntries = predefinedProperties.map((key) => [
+        key,
+        "",
+    ]);
+    const mergedPredefinedEntries = defaultPredefinedEntries.map(
+        mergeWith(nonStylingProperties)
+    );
+
+    const { time, ...predefinedProps } = Object.fromEntries(
+        mergedPredefinedEntries
+    );
+    const { timeStart, timeEnd } = propertiesTimePeriodToForm(time);
+
+    const customEntries = Object.entries(nonStylingProperties)
+        .filter(filterPredefinedEntries)
+        .filter(filterIgnoredEntries);
+    const customProps = customEntries.map(([key, value]) => ({
+        name: key,
+        value,
+    }));
+
+    const styleProps = extractStyleProperties(feature).map(
+        ({ name, value }) => {
+            const { inputProps } = styleFieldSettings[name];
+            const coercedValue = validateStyleValue(value, inputProps);
+            return {
+                name,
+                value: coercedValue,
+            };
+        }
+    );
+
+    return {
+        timeStart,
+        timeEnd,
+        ...predefinedProps,
+        customProps,
+        styleProps,
+    };
+};
+
+/**
+ * Maps form properties to feature properties
+ *
+ * @param formData
+ * @return {Object.<string, any>}
+ */
+export const formPropertiesToFeatureProperties = (formData) => {
+    const {
+        img_link,
+        title,
+        description,
+        timeStart,
+        timeEnd,
+        customProps,
+        styleProps,
+    } = formData;
+
+    const time = formTimePeriodToProperties(timeStart, timeEnd);
+
+    const featureCustomProps = Object.fromEntries(
+        customProps
+            .filter((el) => el.name !== "" || el.value !== "")
+            .map((el) => [el.name, el.value])
+    );
+    const featureStyleProps = Object.fromEntries(
+        styleProps.map((el) => {
+            const { inputProps } = styleFieldSettings[el.name];
+            const coercedValue = validateStyleValue(el.value, inputProps);
+            return [el.name, coercedValue];
+        })
+    );
+
+    return {
+        ...(time.length > 0 && { time }),
+        title,
+        description,
+        img_link,
+        ...featureCustomProps,
+        ...featureStyleProps,
+    };
+};
+
+export const validateStyleValue = (inputValue, inputProps) => {
+    const { type, min, max } = inputProps;
+    let newValue = inputValue;
+
+    if (type === "number") {
+        newValue = Number.isNaN(Number.parseFloat(newValue))
+            ? 1
+            : parseFloat(newValue);
+        newValue = isDefined(min) ? Math.max(min, newValue) : newValue;
+        newValue = isDefined(max) ? Math.min(max, newValue) : newValue;
+    } else if (type === "color") {
+        try {
+            newValue = parseColorStringToHexWithLegacy(newValue);
+        } catch {
+            newValue = DEFAULT_STYLE_VALUES[GEOJSON_LAYER_TYPES.SYMBOL].COLOR;
+        }
+    }
+    return newValue;
+};
+
+export const isValidCustomPropertyFieldName = (value, fields, index) => {
+    const fieldValue = fields.customProps[index].value;
+    const fieldName = value;
+    const otherCustomFieldNames = fields.customProps
+        .filter((_, elIndex) => elIndex !== index)
+        .filter((el) => el.name !== "")
+        .map((el) => el.name.toLowerCase());
+
+    const invalidFieldNames = [
+        ...predefinedProperties,
+        ...otherCustomFieldNames,
+    ];
+
+    if (fieldValue.trim().length > 0 && fieldName.trim() === "") {
+        return false;
+    }
+
+    if (invalidFieldNames.includes(fieldName.toLowerCase())) {
+        return false;
+    }
+
+    return true;
+};
+
+export const isValidCustomPropertyFieldValue = (value, fields, index) => {
+    const fieldName = fields.customProps[index].name;
+
+    if (fieldName.trim().length > 0 && value.trim() === "") {
+        return false;
+    }
+
+    return true;
+};

Build/src/apps/map/components/CustomLayers/GeoJsonLayer/IdohistMapInteractionStrategy/IdohistMapInteractionStrategy.js
@@ -188,15 +188,16 @@ class IdohistMapInteractionStrategy extends AbstractMapInteractionStrategy {
 
     setFilters(map, filterValues) {
         for (const marker of this.#markers) {
-            const time = marker[FEATURE_PROPS_SYMBOL][FEATURE_PROPERTIES.time];
+            const [start, end] =
+                marker[FEATURE_PROPS_SYMBOL][FEATURE_PROPERTIES.time];
             const { timeExtent } = filterValues;
 
-            if (timeExtent[0] * 1000 > time || timeExtent[1] * 1000 < time) {
+            // if time ranges overlap re-add the marker to the map
+            if (start <= timeExtent[1] * 1000 && end >= timeExtent[0] * 1000) {
+                marker.addTo(map);
+            } else {
                 marker.remove();
-                continue;
             }
-
-            marker.addTo(map);
         }
     }
 

Build/src/apps/map/components/GeoJson/GeoJsonFeatureDrawLoader/GeoJsonFeatureDrawLoader.jsx
@@ -16,6 +16,7 @@ import useGeoJsonFeatureDraw, {
 } from "./useGeoJsonFeatureDraw";
 
 import { DRAW_MODE_PANEL_STATE } from "@map/layouts/util";
+import { FEATURE_PROPERTIES } from "../constants";
 
 const USER_PROPERTY_PREFIX = "user_";
 const DRAW_LAYER = {
@@ -54,6 +55,12 @@ const getApplicationFeature = (drawFeature) => {
     .filter(([key]) => key.startsWith(USER_PROPERTY_PREFIX))
     .map(([key, value]) => {
       const userKey = key.slice(USER_PROPERTY_PREFIX.length);
+
+      // time is a stringified array
+      if (userKey === FEATURE_PROPERTIES.time) {
+        value = JSON.parse(value);
+      }
+
       return [userKey, value];
     });
 

Build/src/apps/map/components/GeoJson/GeoJsonFeatureEditPanel/AddFieldButton/AddFieldButton.jsx
@@ -1,0 +1,37 @@
+/*
+ * Created by tom.schulze@pikobytes.de on 23.06.25.
+ *
+ * This file is subject to the terms and conditions defined in
+ * file "LICENSE.txt", which is part of this source code package.
+ */
+
+import React from "react";
+import CustomButton from "../../components/CustomButton";
+import VkfIcon from "@components/VkfIcon";
+import { translate } from "@util/util";
+import PropTypes from "prop-types";
+
+import "./AddFieldButton.scss";
+
+const AddFieldButton = ({ onClick }) => {
+  return (
+    <div className="add-field-button-root">
+      <CustomButton
+        className="add-field-button"
+        buttonType="button"
+        onClick={onClick}
+      >
+        <VkfIcon name="add" />
+        <p className="add-field-button-text">
+          {translate("geojson-editfeature-add-new-field-button")}
+        </p>
+      </CustomButton>
+    </div>
+  );
+};
+
+AddFieldButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddFieldButton;

Build/src/apps/map/components/GeoJson/GeoJsonFeatureEditPanel/AddFieldButton/AddFieldButton.scss
@@ -1,0 +1,58 @@
+/*
+ * Created by tom.schulze@pikobytes.de on 23.06.25.
+ * 
+ * This file is subject to the terms and conditions defined in
+ * file "LICENSE.txt", which is part of this source code package.
+ */
+
+@use "sass:color";
+@use "../../../../Variables" as *;
+
+.add-field-button-root {
+    display: flex;
+    justify-content: center;
+    height: auto;
+
+    .add-field-button {
+        display: flex;
+        gap: 5px;
+        align-items: center;
+        background-color: transparent;
+
+        &:hover,
+        &:focus,
+        &:active,
+        &:active:focus {
+            outline: none;
+            box-shadow: none;
+            background-color: transparent;
+            color: color.adjust($secondary-color-dark, $lightness: -15%);
+
+            .add-field-button-text {
+                color: color.adjust($secondary-color-dark, $lightness: -15%);
+            }
+
+            svg {
+                fill: color.adjust($secondary-color-dark, $lightness: -15%);
+            }
+        }
+
+        .add-field-button-text {
+            color: $secondary-color-dark;
+            font-family: $font;
+            font-size: 12px;
+            font-style: normal;
+            font-weight: 700;
+            line-height: 26px;
+            letter-spacing: 0.48px;
+            text-transform: uppercase;
+            margin: unset;
+        }
+
+        svg {
+            fill: $secondary-color-dark;
+            width: 18px;
+            height: 18px;
+        }
+    }
+}

Build/src/apps/map/components/GeoJson/GeoJsonFeatureEditPanel/AddFieldButton/index.js
@@ -1,0 +1,10 @@
+/*
+ * Created by tom.schulze@pikobytes.de on 23.06.25.
+ *
+ * This file is subject to the terms and conditions defined in
+ * file "LICENSE.txt", which is part of this source code package.
+ */
+
+import AddFieldButton from "./AddFieldButton";
+
+export default AddFieldButton;

commit 39bc7a3dfade61266cad7503b7c6693e0c72ed38

Do not persist GeoJson in application state for remote vector maps (#495)
* Fix geojson is persisted in app state
- Don't store geojson in application state in the database, store in persistance state

* Fix bug introduced in #483

Build/src/apps/map/components/GeoJson/util/hooks/useSaveGeoJson.js
@@ -26,8 +26,8 @@ import {
 import { GeoJsonLayer, METADATA } from "@map/components/CustomLayers";
 import {
     exitDrawMode,
-    processNewGeoJsonForPersistence,
-    processUpdatedGeoJsonForPersistence,
+    generateStableIdsForNewGeojson,
+    generateStableIdsForUpdatedGeojson,
     handleErrorResponse,
     removeAllFeatureIds,
 } from "@map/components/GeoJson/util/util";
@@ -48,7 +48,7 @@ export const useSaveGeoJson = () => {
             async () => {
                 const draw = await snapshot.getPromise(drawState);
                 if (draw) {
-                    const geoJson = processNewGeoJsonForPersistence(
+                    const geoJson = generateStableIdsForNewGeojson(
                         draw.getAll()
                     );
                     const map = await snapshot.getPromise(mapState);
@@ -60,8 +60,10 @@ export const useSaveGeoJson = () => {
                     let id = null;
                     try {
                         // persist vector map to remote
+                        const processedGeojson =
+                            GeoJsonLayer.toPersistenceState(geoJson);
                         id = await createNewVectorMap(
-                            geoJson,
+                            processedGeojson,
                             metadataDrawToApi(metadata)
                         );
                     } catch (error) {
@@ -144,17 +146,18 @@ export const useSaveGeoJson = () => {
                     }
 
                     const nextVersion = vectorMapDraw.version + 1;
-                    const processedGeoJson =
-                        processUpdatedGeoJsonForPersistence(
-                            initialGeoJson,
-                            geoJsonFromDraw,
-                            nextVersion
-                        );
+                    const geoJson = generateStableIdsForUpdatedGeojson(
+                        initialGeoJson,
+                        geoJsonFromDraw,
+                        nextVersion
+                    );
                     const metadataToSend = metadataDrawToApi(metadata);
 
                     let newVersion = null;
                     try {
                         // Only push geojson and metdata, if there are changes
+                        const processedGeoJson =
+                            GeoJsonLayer.toPersistenceState(geoJson);
                         newVersion = await updateVectorMap(
                             vectorMapDraw.id,
                             hasGeoJsonChanged ? processedGeoJson : null,
@@ -174,7 +177,7 @@ export const useSaveGeoJson = () => {
                         // update application layer geojson
                         // update application layer version
                         if (hasGeoJsonChanged) {
-                            selectedLayer.setDataOnMap(map, processedGeoJson);
+                            selectedLayer.setDataOnMap(map, geoJson);
                             selectedLayer.updateMetadata(
                                 METADATA.version,
                                 newVersion

Build/src/apps/map/components/GeoJson/util/initializeVectorMap.js
@@ -26,7 +26,9 @@ const initializePersistentVectorMap = async (layer) => {
     layer.updateMetadata(METADATA.userRole, vectorMap[METADATA.userRole]);
     layer.updateMetadata(METADATA.version, vectorMap[METADATA.version]);
 
-    layer.setGeoJson(vectorMap.geojson);
+    const geojson = GeoJsonLayer.toApplicationState(vectorMap.geojson);
+
+    layer.setGeoJson(geojson);
 };
 
 const initializeVectorMap = async (layer) => {

Build/src/apps/map/components/GeoJson/util/util.js
@@ -156,7 +156,7 @@ const generateFeatureID = (idx, version) => {
  * @param {*} geoJson
  * @returns {*} geoJson with stable ids
  */
-export const processNewGeoJsonForPersistence = (geoJson) => {
+export const generateStableIdsForNewGeojson = (geoJson) => {
     const clonedGeoJson = structuredClone(geoJson);
 
     for (const [idx, feature] of clonedGeoJson.features.entries()) {
@@ -174,7 +174,7 @@ export const processNewGeoJsonForPersistence = (geoJson) => {
  * @param {number} newVersion
  * @returns {*} geoJson with stable ids
  */
-export const processUpdatedGeoJsonForPersistence = (
+export const generateStableIdsForUpdatedGeojson = (
     initialGeoJson,
     newGeoJson,
     newVersion